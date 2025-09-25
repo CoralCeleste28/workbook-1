2431304b7e6823e1b6e5d519bec3d4c5e8ecd8be
Coral Estrada <coralestrada28@gmail.com>
2025-09-25 16:38:06 -0400

    Inital Commit

---
+++ SandwichShop/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                a. 1: Regular: base price $5.45
+                b. 2: Large: base price $8.95
+          """);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
